9441d8599b94817468427c0739357f6c0aae6dc4
sanchit <sanchit.astekar@inetframe.com>
2021-12-01 14:05:42 +0530

    tminor changes 3

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello! This is the world of Information Technology')
 })
 
+app.get('/newfun1', (req, res) => {
+    res.send('This is new function1')
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
