83f6169a36ac86f5b28b28deffe6b6182f9c99ec
sanchit <sanchit.astekar@inetframe.com>
2021-12-01 14:20:29 +0530

    3 tmajor commit

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello! This is the world of Information Technology')
 })
 
-app.get('/newfun2', (req, res) => {
-    res.send('This is new function-2')
-  })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
