9002b7d1f584575d7bd7f300fb4daede7140e77e
sanchit <sanchit.astekar@inetframe.com>
2021-12-01 14:24:38 +0530

    third changes

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ app.get('/', (req, res) => {
 
 app.get('/newfun2', (req, res) => {
     res.send('This is new function-2')
-  })
+  });
+
+  app.get('/newfun3', (req, res) => {
+    res.send('This is new function-3')
+  });  
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
